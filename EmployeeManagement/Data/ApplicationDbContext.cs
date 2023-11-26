f53f8b0454ebef373b8e936e59f56eff12d7ce2b
NeoMatlala <neomatlala70@gmail.com>
2023-11-26 08:11:05 +0200

    Departments model, migrations & DB table

--- EmployeeManagement/Data/ApplicationDbContext.cs
+++ EmployeeManagement/Data/ApplicationDbContext.cs
@@ -11,5 +11,6 @@ namespace EmployeeManagement.Data
         }
 
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
     }
 }
---
+++ EmployeeManagement/Migrations/20231126060721_AddDepartmentsToDB.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartmentsToDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Manager = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Members = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
